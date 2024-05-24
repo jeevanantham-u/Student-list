06a02092a952e3a70df5df7006a367aa7ed68141
jeevanantham-u <jeevanantham2k2@gmail.com>
2024-05-24 18:58:38 +0530

    all api are updated

--- index.js
+++ index.js
@@ -33,94 +33,84 @@ const studentListSchima = new mongoose.Schema({
 
 const Student = mongoose.model('Student', studentListSchima);
 
+console.log();
+
 app.get('/students', async(req, res) => {
-    const students = await Student.find({});
+    const students = await Student.find();
     res.json(students);
 });
 
-app.post('/students', (req, res) => {
-    const newStudent = new Student({
-        name: req.body.name,
-        dob: req.body.dob,
-        class: req.body.class,
-        phone: req.body.phone,
-        address: req.body.address
-    });
-    list.push(newStudent);
-    newStudent.save();
-    res.json(newStudent);
-});
-
-app.put('/students/:id', (req, res) => {
-   const id = parseInt(req.params.id); 
-   const replacementData = {
-    id: id,
-    name: req.body.name,
-    dob: req.body.dob,
-    class: req.body.class,
-    phone: req.body.phone,
-    address: req.body.address,
-   };
-   const searchIndex = list.findIndex((data) => { data.id === id });
-   list[searchIndex] = replacementData ;
-   res.json(replacementData);
-});
-
-app.delete('/students/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const searchIndex = list.findIndex(data => data.id === id );
-    if (searchIndex > -1){
-        list.splice(searchIndex, 1);
-        res.sendStatus(200);
-    } else {
-        res.status(404).json({
-            error: `Student with id: ${id} not found. No student were deledted`
+app.post('/students', async (req, res) => {
+    try {
+        // Check if request body exists
+        if (!req.body || Object.keys(req.body).length === 0) {
+            throw new Error('Request body is empty');
+        }
+    
+        const newStudent = new Student({
+            name: req.body.name,
+            dob: req.body.dob,
+            class: req.body.class,
+            phone: req.body.phone,
+            address: req.body.address
         });
+        
+        newStudent.save();
+        res.status(201).json(newStudent);
+    } catch (err) {
+            // Handle errors
+            console.error('Error creating document:', err.message);
+            res.status(400).json({ error: `Document not created. ${err.message}` });
     }
 });
+    
+app.put('/students/:id', async(req, res) => {
+   const id = req.params.id; 
+   try {
+        if (!req.body || Object.keys(req.body).length === 0) {
+            throw new Error('Request body is empty');
+        }
+
+        const replacementData = {
+            name: req.body.name,
+            dob: req.body.dob,
+            class: req.body.class,
+            phone: req.body.phone,
+            address: req.body.address,
+           };
+
+        const updatedData = await Student.findByIdAndUpdate(id, replacementData, { new: true });
+
+        if (!updatedData) {
+            throw new Error('Document not found');
+        }
+
+        res.json(updatedData);
+    } catch (err) {
+        // Handle errors
+        console.error('Error updating document:', err.message);
+        res.status(400).json({ error: `Document not updated. ${err.message}` });
+    }
+});
+
+app.delete('/students/:id', async(req, res) => {
+    const id = req.params.id;
+    try {
+        const deleteData = await Student.findByIdAndDelete(id);
+        console.log(deleteData);
+
+        if (!deleteData) {
+          return res.status(404).json({ message: 'Id not found' });
+        }
+
+        res.status(200).json({ message: 'data deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});    
+
 
 app.listen(port, () => {
     console.log(`server running on port:${port}`);
 });
 
-const list = [
-    {
-        id: 1,
-        name: 'jeeva',
-        dob: '10/02/2002',
-        class: '10',
-        phone: '7603982326',
-        address: 'madurai'
-    },
-    {
-        id: 2,
-        name: 'mari',
-        dob: '12/05/2002',
-        class: '10',
-        phone: '7603982326',
-        address: 'madurai'
-    },
-    {
-        id: 3,
-        name: 'arun',
-        dob: '29/02/2001',
-        class: '11',
-        phone: '7603982326',
-        address: 'madurai'
-    },
-    {
-        id: 4,
-        name: 'vicky',
-        dob: '05/02/2000',
-        class: '12',
-        phone: '7603982326',
-        address: 'madurai'
-    },
-    {
-        id: 5,
-        name: 'Praveen',
-        dob: '18/08/2002',
-        phone: '7603982326',
-        address: 'madurai'
-    },
-];
